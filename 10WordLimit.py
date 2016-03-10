4a8e14623c5e780bcf0a6ae553fd3dddb2c169c6
Jack Wolfson <jwolfson2017@pingry.org>
2016-03-09 23:18:46 -0500

    This is the final running 10 word limit with input and output txt

--- 10WordLimit.py
+++ 10WordLimit.py
@@ -1,18 +1,34 @@
 f = open("input.txt", 'r')
-lines = f.read()
-length = f.tell()
+lines = f.read().splitlines()
+# find a program that makes each line a variable
 
 f.close()
 
 print(lines)
 
 f = open("output.txt", 'r+')
-f.write(lines)
+
+for line in lines:
+	length = len(line)
+	if length >	10:
+		f.write(line[0:10])
+		newLine = line[0:10]
+		print(newLine)
+	if length <=10:
+		f.write(line)
+		print(line)
 
 f.close()
 
-print(lines)
 
-
-# if length <10
-	
+# open the input file & read
+# read the lines of the file
+# read the length of each individual line in the file
+# close input file
+# print the lines
+# open the output text & read/write
+# write the lines of the input into the output
+# if the length of a line is less then 10, print line
+# run this for every line
+# if the length is greater then 10, delete letters until length == 10
+# f.close()
